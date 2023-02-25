3ae788924ae4a5aafe5de32285d46c49521bd898
Felipe Cuba <felipeccuba1803@gmail.com>
2023-02-25 11:47:18 -0300

    refactor(teste):new param to sum

--- Aula3/index.php
+++ Aula3/index.php
@@ -11,12 +11,12 @@ class Teste
         $this->b = $b;
     }
 
-    public function getSoma()
+    public function getSoma(int $c)
     {
-        return $this->a + $this->b;
+        return $this->a + $this->b + $c;
     }
 }
 
 $teste = new Teste(10, 30);
 
-echo $teste->getSoma();
+echo $teste->getSoma(50);
