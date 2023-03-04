aa38facd4a989b90d6dfe3fb93f7e5a20c4f0818
Felipe Cuba <felipeccuba1803@gmail.com>
2023-03-04 12:30:43 -0300

    wip(exercicio-aula4)

--- Aula4/Exercicio/index.php
+++ Aula4/Exercicio/index.php
@@ -2,28 +2,26 @@
 
 // Implemente através do mecanismo de herança, um cadastro de pessoas 
 // em uma faculdade, em uma faculdade podemos ter três tipos 
-// de pessoas: professor, funcionário e aluno. O cadastro de professores 
-// deve armazenar as informações de cpf, nome, email, matricula, carga horario, 
-// salario e departamento. O cadastro de funcionarios deve armazenar as 
-// informações de nome, cpf, email, matricula, regime, salario. O cadastro de 
-// alunos deve armazenar as informações de nome, cpf, email, ra, curso e termo 
-// que se encontra. Os professores e os funcionários devem possuir um método 
+// de pessoas: professor, funcionário e aluno. Os professores e os funcionários devem possuir um método 
 // que atualiza seus salários e os alunos um método que altere o seu termo. 
 // Deve existir também um método para retornar os dados da pessoa, retornando 
 // um objeto com as informações nome, cpf e email.
 // Implemente e teste as classes derivadas.
 
-class Pessoa
+abstract class Pessoa
 {
-    private string $cpf;
-    private string $email;
-    private string $nome;
+    protected string $cpf;
+    protected string $email;
+    protected string $nome;
 
     function __construct(string $cpf, string $email, string $nome)
     {
+        $this->setCpf($cpf);
+        $this->setEmail($email);
+        $this->setNome($nome);
     }
 
-    protected function setCpf(string $cpf)
+    private function setCpf(string $cpf)
     {
         if (gettype($cpf) == "string") {
             $this->cpf = $cpf;
@@ -33,7 +31,7 @@ class Pessoa
         }
     }
 
-    protected function setEmail(string $email)
+    private function setEmail(string $email)
     {
         if (gettype($email) == "string") {
             $this->email = $email;
@@ -43,7 +41,7 @@ class Pessoa
         }
     }
 
-    protected function setNome(string $nome)
+    private function setNome(string $nome)
     {
         if (gettype($nome) == "string") {
             $this->nome = $nome;
@@ -53,8 +51,193 @@ class Pessoa
         }
     }
 
-    public function getData()
+    public function getDetalhes(): string
     {
         return "Nome: {$this->nome} | Email: {$this->email} | CPF: {$this->cpf}";
+    }
+}
+
+
+/* O cadastro de professores 
+deve armazenar as informações de cpf, nome, email, matricula, carga horario, 
+salario e departamento. */
+final class Professor extends Pessoa
+{
+    private string $matricula;
+    private int $carga_horaria;
+    private float $salario;
+    private string $departamento;
+
+    function __construct(
+        string $cpf,
+        string $email,
+        string $nome,
+        string $matricula,
+        int $carga_horaria,
+        float $salario,
+        string $departamento
+    )
+    {
+        parent::__construct($cpf, $email, $nome);
+        $this->setCargaHoraria($carga_horaria);
+        $this->setMatricula($matricula);
+        $this->setSalario($salario);
+        $this->setDepartamento($departamento);
+    }
+
+    private function setMatricula(string $matricula)
+    {
+        if (gettype($matricula) == "string") {
+            $this->matricula = $matricula;
+        } else {
+            echo "Valor digitado para a matricula é inválido!";
+        }
+    }
+    private function setCargaHoraria(int $carga_horaria)
+    {
+        if (gettype($carga_horaria) == "int") {
+            $this->carga_horaria = $carga_horaria;
+        } else {
+            echo "Valor digitado para a carga horária é inválido!";
+        }
+    }
+
+    private function setSalario(float $salario)
+    {
+        if (gettype($salario) == "double") {
+            $this->salario = $salario;
+        } else {
+            echo "Valor digitado para o salário é inválido!";
+        }
+    }
+
+    private function setDepartamento(string $departamento)
+    {
+        if (gettype($departamento) == "string") {
+            $this->departamento = $departamento;
+        } else {
+            echo "Valor digitado para o departamento é inválido!";
+        }
+    }
+
+    public function atualizaSalario(float $indice): bool
+    {
+        $this->salario += $this->salario * $indice / 100;
+
+        return true;
+    }
+}
+
+/*
+O cadastro de funcionarios deve armazenar as informações de nome, cpf, email, matricula, regime, salario.
+*/
+
+final class Funcionario extends Pessoa
+{
+    private string $matricula;
+    private string $regime;
+    private float $salario;
+
+    function __construct(
+        string $cpf,
+        string $email,
+        string $nome,
+        string $matricula,
+        string $regime,
+        float $salario
+    )
+    {
+        parent::__construct($cpf, $email, $nome);
+        $this->setMatricula($matricula);
+        $this->setRegime($regime);
+        $this->setSalario($salario);
+
+    }
+
+    private function setMatricula(string $matricula): void
+    {
+        if (gettype($matricula) == "string") {
+            $this->matricula = $matricula;
+        } else {
+            echo "Valor digitado para a matricula é inválido!";
+        }
+    }
+
+    private function setSalario(float $salario): void
+    {
+        if (gettype($salario) == "double") {
+            $this->salario = $salario;
+        } else {
+            echo "Valor digitado para o salario é inválido!";
+        }
+    }
+
+    private function setRegime(string $regime): void
+    {
+        if (gettype($regime) == "string") {
+            $this->regime = $regime;
+        } else {
+            echo "Valor digitado para o regime é inválido!";
+        }
+    }
+
+    public function atualizaSalario(float $indice): bool
+    {
+        $this->salario += $this->salario * $indice / 100;
+
+        return true;
+    }
+}
+
+/* O cadastro de 
+alunos deve armazenar as informações de nome, cpf, email, ra, curso e termo 
+que se encontra */
+
+final class Aluno extends Pessoa
+{
+    private string $ra;
+    private string $curso;
+    private int $termo;
+
+    function __construct(
+        string $cpf,
+        string $email,
+        string $nome,
+        string $ra,
+        string $curso,
+        int $termo
+    )
+    {
+        parent::__construct($cpf, $email, $nome);
+        $this->setCurso($curso);
+        $this->setTermo($termo);
+        $this->setRa($ra);
+    }
+
+    private function setRa(string $ra): void
+    {
+        if (gettype($ra) == "string") {
+            $this->ra = $ra;
+        } else {
+            echo "Valor digitado para o R.A é inválido";
+        }
+    }
+
+    private function setCurso(string $curso): void
+    {
+        if (gettype($curso) == "string") {
+            $this->curso = $curso;
+        } else {
+            echo "Valor digitado para o curso é inválido";
+        }
+    }
+
+    private function setTermo(int $termo): void
+    {
+        if (gettype($termo) == "string") {
+            $this->termo = $termo;
+        } else {
+            echo "Valor digitado para o termo é inválido";
+        }
     }
 }
