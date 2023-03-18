dda0eb05a0efbf1e0abe340ebbf0ce384cff8ec3
Felipe Cuba <felipeccuba1803@gmail.com>
2023-03-17 22:05:59 -0300

    aula-6-connection

---
+++ Aula6/index.php
@@ -0,0 +1,23 @@
+<?php
+require_once('config/connection.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste</title>
+</head>
+
+<body>
+    <form action="">
+        <input type="text" name="nome" id="nome">
+        <input type="text" name="email" id="email">
+        <input type="text" name="tipo" id="tipo">
+        <input type="textarea" name="mensagem" id="mensagem">
+    </form>
+</body>
+
+</html>
